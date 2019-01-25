920b3cd174dd26f6870ac1502a1de05fe269f06e
sakthipoonga06 <46962125+sakthipoonga06@users.noreply.github.com>
2019-01-25 23:33:44 +0530

    Create pack1.py

---
+++ pack1.py
@@ -0,0 +1,12 @@
+year=int(input())
+if year%4==0:
+    if year%100==0:
+        if year%400==0:
+            print("leap")
+        else:
+            print("no")
+    else:
+        print("leap")
+else:
+    print("no")
+                       
